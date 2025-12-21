5ebb56889b0cc53538bb3c34b767afea5cf60f41
arbans1 <arbans1@gmail.com>
2025-12-21 12:54:04 +0900

    feat: GraphQL 스키마 및 사용자 인증 관련 타입 추가
    - GraphQL 스키마에 쿼리 및 뮤테이션 타입 정의
    - 사용자 관련 입력 및 타입 정의 추가
    - 사용자 역할 및 상태를 나타내는 Enum 추가
    - 커스텀 에러 및 페이지 정보 타입 정의
    - 커스텀 스칼라 타입(DateTime) 추가
    - 권한 제어를 위한 지시자 및 Enum 정의

---
+++ src/main/java/com/example/bbs/config/GraphqlConfig.java
@@ -0,0 +1,20 @@
+package com.example.bbs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+import graphql.scalars.ExtendedScalars;
+
+@Configuration
+public class GraphqlConfig {
+
+	@Bean
+	public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+		return wiringBuilder -> {
+			wiringBuilder
+				.scalar(ExtendedScalars.DateTime);
+		};
+	}
+
+}
